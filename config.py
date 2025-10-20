4cd8995ec832c7bd7b907fcdf8a18c6a61912f2a
mrsargio <mrsargio5@gmail.com>
2025-10-20 13:59:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,17 @@
 import os
 
+# Environment variables set karna
+os.environ["BOT_TOKEN"] = "6047785902:AAE59KTfmhRvF8sUSYIzl9wcGnm4FLXiWDk"
+os.environ["API_ID"] = "24250238"
+os.environ["API_HASH"] = "cb3f118ce5553dc140127647edcf3720"
+os.environ["VIP_USER"] = "6175650047"
+
 class Config(object):
-    # Yeh sirf variable ke naam hone chahiye, values export se aayengi
+    # Bot configuration variables
     BOT_TOKEN = os.environ.get("BOT_TOKEN")
     API_ID = int(os.environ.get("API_ID"))
     API_HASH = os.environ.get("API_HASH")
     
+    # VIP users ko handle karna
     VIP_USER = os.environ.get("VIP_USER", '')
-    VIP_USERS = [int(user_id) for user_id in VIP_USER.split(',') if user_id]
+    VIP_USERS = [int(user_id.strip()) for user_id in VIP_USER.split(',') if user_id.strip()]
